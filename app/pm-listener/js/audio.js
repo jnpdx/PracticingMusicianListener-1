afaff8f3f6d57c3766563dd27e04b0b026dc41a9
jnpdx <john@johnnastos.com>
2018-05-15 09:14:17 -0700

    Change buffer duration

--- app/pm-listener/js/audio.js
+++ app/pm-listener/js/audio.js
@@ -10,7 +10,7 @@ var AudioAnalyzer = function() {
     this.sourceNode = null;
     this.analyzer = null;
     this.audioContext = null;
-    this.buflen = 4096;
+    this.buflen = 1024;
     this.buf = new Float32Array( this.buflen );
     this.rafID = null;
 
@@ -69,13 +69,9 @@ var AudioAnalyzer = function() {
 
         // Connect it to the destination.
         this.analyzer = this.audioContext.createAnalyser();
-        //this.analyzer.fftSize = 2048;
-        this.scriptProcessor = this.audioContext.createScriptProcessor(4096, 1, 1)
+        this.scriptProcessor = this.audioContext.createScriptProcessor(1024, 1, 1)
         this.pitchDetector = new (Module().AubioPitch)(
-          'default', 4096, 1, this.audioContext.sampleRate)
-
-//        mediaStreamSource = this.audioContext.createMediaStreamSource(stream);
-//        mediaStreamSource.connect( this.analyzer );
+          'default', 1024, 1, this.audioContext.sampleRate)
 
       this.audioContext.createMediaStreamSource(stream).connect(this.analyzer)
       this.analyzer.connect(this.scriptProcessor)
@@ -92,26 +88,6 @@ var AudioAnalyzer = function() {
       return this.storedPitch
     }
     this.storedPitch = -1
-
-    //get the current pitch
-    this.OLD_updatePitch = function(timestamp) {
-        if (this.analyzer == null)
-            return
-
-        //pm_log("Updating pitch")
-
-        var cycles = new Array;
-        this.analyzer.getFloatTimeDomainData( this.buf );
-
-        //calculate the amplitude
-        //var avg = this.buf.reduce(function(sum, a) { return sum + a },0)/(this.buf.length||1);
-        //console.log("Average: " + avg)
-
-        var ac = this.autoCorrelate( this.buf, this.audioContext.sampleRate );
-
-        return ac
-    }
-
 
     this.autoCorrelate = function(a,r){var e=a.length;var t=Math.floor(e/2);var n=-1;var o=0;var u=0;var i=false;var l=new Array(t);for(var v=0;v<e;v++){var f=a[v];u+=f*f}u=Math.sqrt(u/e);if(u<thresholdAmount)return-1;var c=1;for(var s=MIN_SAMPLES;s<t;s++){var d=0;for(var v=0;v<t;v++){d+=Math.abs(a[v]-a[v+s])}d=1-d/t;l[s]=d;if(d>GOOD_ENOUGH_CORRELATION&&d>c){i=true;if(d>o){o=d;n=s}}else if(i){var m=(l[n+1]-l[n-1])/l[n];return r/(n+8*m)}c=d}if(o>.01){return r/n}return-1}
 }
